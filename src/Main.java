96ea84acc442efeb4ff7c81957f8db6a449fb362
pinku_protogen <66691079+MatiiFurry7w7@users.noreply.github.com>
2021-05-31 20:39:45 -0300

    Revert "Segunda prueba commit IntelIJ"
    This reverts commit 67109f432aaea8a957f970ad12f9f89f1edfe932.

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ public class Main {
         Client mati = new Client(true, "Matias", "2234725542", "mati@gmail.com", "Alberdi 7777", false);
 
         Cart matiCart = new Cart();
-        String a = "HOLA";
 
 
     }

f90f6477933535e3e230d74ec5d1be2bed342027
Martín <caceres.martin@live.com.ar>
2021-06-01 18:01:15 -0300

    Actualización Alcoholic

--- src/products/drinks/Alcoholic.java
+++ src/products/drinks/Alcoholic.java
@@ -1,23 +1,56 @@
 package products.drinks;
 
-public class Alcoholic {
+
+public class Alcoholic extends Drink {
 
     private typeAlcoholic type;
     private float alcoholicStrength;
 
-    public Alcoholic(){};
-    public Alcoholic(String brand, int stock, float price, float size, String bottling, boolean fizz, String flavor, typeAlcoholic type){};
+    public Alcoholic(){
+        //falta que martin termine la interface de IDProducto
+
+    };
+
+    public Alcoholic(String brand, int stock, float price, float size, String bottling, boolean fizz, String flavor, typeAlcoholic type, float strength){
+
+        super.setBrand(brand);
+        super.setStock(stock);
+        super.setPrice(price);
+        super.setSize(size);
+        super.setBottling(bottling);
+        super.setFizz(fizz);
+        super.setFlavor(flavor);
+
+        this.type = type;
+        this.alcoholicStrength = strength;
+    };
+
+    //region GET/SET
+    public typeAlcoholic getType() {
+        return type;
+    }
+
+    public void setType(typeAlcoholic type) {
+        this.type = type;
+    }
+
+    public float getAlcoholicStrength() {
+        return alcoholicStrength;
+    }
+
+    public void setAlcoholicStrength(float alcoholicStrength) {
+        this.alcoholicStrength = alcoholicStrength;
+    }
+
+    //endregion
 
     @Override
     public String toString(){
-        return "ID: " + id +
-                "\nName: " + super.getName() +
-                "\nPhone: " + super.getPhone() +
-                "\nE-mail: " + super.getEmail() +
-                "\nAddress: " + super.getAddress() +
-                "\nRegistered date: " + super.getRegisteredDate() +
-                "\nPremium: " + premium +
-                "\nOrders amount: " + ordersAmount;
+        return "Brand: " + super.getBrand() +
+                "\nStock: " + super.getStock() +
+                "\nPrice: " + super.getPrice() +
+                "\nBottling: " + super.getBottling() +
+                "\nFizz: " + super.getFlavor();
     };
 
 

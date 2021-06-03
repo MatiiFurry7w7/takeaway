0e3e043ed0750d1d41aaded134ceaa25c856a3cd
Martin Garat <martin.garat59@gmail.com>
2021-06-02 23:21:45 -0300

    ID Product, Meat Class

--- src/products/food/Food.java
+++ src/products/food/Food.java
@@ -1,4 +1,47 @@
 package products.food;
 
 public abstract class Food {
+
+    private String name;
+    private float price;
+    private String description;
+    private int numberPeopleRecommended;
+
+
+    //region GET/SET
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getNumberPeopleRecommended() {
+        return numberPeopleRecommended;
+    }
+
+    public void setNumberPeopleRecommended(int numberPeopleRecommended) {
+        this.numberPeopleRecommended = numberPeopleRecommended;
+    }
+
+    //endregion
+
+    
 }
